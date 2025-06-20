2eae20e6d1455d2a10cbd1aa6a5850c8885773d3
yuvraj1235 <reignitedyuvraj@gmail.com>
2025-06-20 15:57:50 +0530

    dbconnect updated

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,22 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-
-export async function connect() {
+export async function dbConnect(){
     try {
-        mongoose.connect(process.env.MONGODB_URL!);
-        const connection =mongoose.connection;
-        connection.on('connected',()=>{
-            console.log("MongoDB connected successfully");
-            
+        mongoose.connect(process.env.MONGODB_URI!)
+
+        const connection = mongoose.connection
+        connection.on("connected", () => {
+            console.log("Mongodb Connected Succesfully")
         })
-        connection.on('error',(err)=>{
-            console.log("MONGO db connection failled:",err);
-            process.exit;
-            
-        })
-    } catch (error) {
-        console.log("Something went wrong whilte db connection");
-        console.log(error);
+
+        connection.on("errors",(err) => {
+            console.log("Error occured while connecting to the mongodb",err)
+            process.exit()
+         })
         
+    } catch (error) {
+        console.log("Something is not right",error)
     }
 }
