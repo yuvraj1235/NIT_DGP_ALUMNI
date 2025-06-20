2dde8add045fe5e183cc03e73876cb61ec52b9e5
yuvraj1235 <reignitedyuvraj@gmail.com>
2025-06-20 15:54:08 +0530

    Routes protected

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  // Paths that don't require auth
+  const isPublicPath = pathname === '/admin/login';
+
+  const token = request.cookies.get('admin-token')?.value;
+
+  // If no token
+  if (!token) {
+    if (!isPublicPath) {
+      return NextResponse.redirect(new URL('/admin/login', request.url));
+    }
+    return NextResponse.next();
+  }
+if(isPublicPath && token) {
+    return NextResponse.redirect(new URL('/', request.nextUrl))
+  }
+
+}
+
+export const config = {
+  matcher: [
+    '/admin/dashboard/:path*',
+  ],
+};
