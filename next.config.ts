81ee9460b0e3d7833c93b449ddb42a6acafeb62d
yuvraj1235 <reignitedyuvraj@gmail.com>
2025-06-20 16:15:22 +0530

    next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 export default nextConfig;
